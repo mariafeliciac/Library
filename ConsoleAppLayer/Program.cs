44fa009a31a9af613d96957ce37673f3786626c1
Maria Felicia Conte <forzafelicia25@gmail.com>
2024-07-11 12:20:19 +0200

    Modifica ulteriore della stringa di connessione ed aggiunta del file json nella ConsoleApp per testarla. Modifica nel program della Console della ConfigurationBuilder.

--- ConsoleAppLayer/Program.cs
+++ ConsoleAppLayer/Program.cs
@@ -12,13 +12,11 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var builder = new ConfigurationBuilder()
-                    .SetBasePath("C:\\Users\\maria.felicia.conte\\source\\repos\\Library - Conte\\Library - Conte\\ConsoleAppLayer")
-                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        var build = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        var configuration = build.Build();
+        string connectionString = configuration.GetConnectionString("LibraryDatabase");
 
-        var configuration = builder.Build();
-        string connectionString = configuration.GetConnectionString("LibraryLocal")!;
-        string connectionString2 = configuration.GetSection("ConnectionStrings")["library"]!;
 
         IDataAccessADO dataAccessADO = new DataAccessADO(connectionString);
         IBusinessLogic businessLogic = new BusinessLogic(new Repository(new UserDAOForADO(dataAccessADO), new BookDAOForADO(dataAccessADO), new ReservationDAOForADO(dataAccessADO))/*,publisherClassEvent*/);
